30e48677fdefdf53723e31a98503cb067b84e78f
Caleb Joseph <calebbjoseph@gmail.com>
2023-12-28 19:56:27 -0600

    working on authentications

--- src/pages/auth/PropertyManagement.js
+++ src/pages/auth/PropertyManagement.js
@@ -5,15 +5,17 @@ import {Link} from "react-router-dom";
 import axios from "axios";
 
 export default function PropertyManagement() {
-    const [name, setName] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
 
     const handleForm = (e) => {
         e.preventDefault();
+        const data = {firstName, lastName, email, password}
 
-        axios.post('//localhost:3000/api/tenant', {firstName: name, lastName: email})
+        axios.post('//localhost:3000/api/tenant', data)
             .then(response => console.log(response))
             .catch(err => console.log(err));
     }
@@ -23,44 +25,80 @@ export default function PropertyManagement() {
                 <div className="bg-white p-3 rounded w-50">
                     <h2>Property Registration</h2>
                     <form onSubmit={handleForm}>
-                        <Input
-                            value={name}
-                            label="Name"
-                            type="text"
-                            placeholder="Enter Name"
-                            name="name"
-                            className="form-control rounded-0"
-                            onChange={e => {
-                                setName(e.target.value)
-                            }}
-                        />
+                        <div className="row">
+                            <div className="col-md-6">
+                                <Input
+                                    value={firstName}
+                                    label="First Name"
+                                    type="text"
+                                    name="firstName"
+                                    placeholder='Enter First name'
+                                    className="form-control rounded-0"
+                                    onChange={e => {
+                                        setFirstName(e.target.value)
+                                    }}
+                                />
+                            </div>
 
-                        <Input
-                            value={email}
-                            label="Email"
-                            type="text"
-                            placeholder="Enter Email"
-                            name="email"
-                            className="form-control rounded-0"
-                            onChange={e => {
-                                setEmail(e.target.value)
-                            }}
-                        />
+                            <div className="col-md-6">
+                                <Input
+                                    value={lastName}
+                                    label="Last Name"
+                                    type="text"
+                                    name="lastname"
+                                    placeholder='Enter Last Name'
+                                    className="form-control rounded-0"
+                                    onChange={e => {
+                                        setLastName(e.target.value)
+                                    }}
+                                />
+                            </div>
 
-                        <Input
-                            value={password}
-                            label="Password"
-                            type="password"
-                            placeholder="Enter Password"
-                            name="password"
-                            className="form-control rounded-0"
-                            onChange={e => {
-                                setPassword(e.target.value)
-                            }}
-                        />
+                            <div className="col-md-12">
+                                <Input
+                                    value={email}
+                                    label="Email"
+                                    type="text"
+                                    placeholder="Enter Email"
+                                    name="email"
+                                    className="form-control rounded-0"
+                                    onChange={e => {
+                                        setEmail(e.target.value)
+                                    }}
+                                />
+                            </div>
+
+                            <div className="col-md-6">
+                                <Input
+                                    value={password}
+                                    label="Password"
+                                    type="password"
+                                    placeholder="Enter Password"
+                                    name="password"
+                                    className="form-control rounded-0"
+                                    onChange={e => {
+                                        setPassword(e.target.value)
+                                    }}
+                                />
+                            </div>
+                            <div className="col-md-6">
+                                <Input
+                                    value={password}
+                                    label="Confirm Password"
+                                    type="password"
+                                    placeholder="Enter Password again"
+                                    name="passwordConfirm"
+                                    className="form-control rounded-0"
+                                    onChange={e => {
+                                        setPassword(e.target.value)
+                                    }}
+                                />
+                            </div>
+                        </div>
                         <Button text="Signup" color={'blue'}/>
-                        <p>Already have an account? <Link style={{textDecoration: "none"}} to={'/login'}>Login</Link></p>
-                        <p><Link style={{ textDecoration: "none"}} to={'/'}>Go back to homePage</Link></p>
+                        <p>Already have an account? <Link style={{textDecoration: "none"}} to={'/login'}>Login</Link>
+                        </p>
+                        <p><Link style={{textDecoration: "none"}} to={'/'}>Go back to homePage</Link></p>
                     </form>
                 </div>
             </div>
