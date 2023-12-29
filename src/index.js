f240fe6eade6cbb755cf42084355c76c0ee366c6
Dare Alesh <damilarealesh99@gmail.com>
2023-12-28 22:18:24 -0600

    user provider utility

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
 import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { UserProvider } from "./utility/UserProvider";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <Router>
-            <App />
-        </Router>
-    </React.StrictMode>,
+  <React.StrictMode>
+    <UserProvider>
+      <Router>
+        <App />
+      </Router>
+    </UserProvider>
+  </React.StrictMode>
 );
 
 reportWebVitals();
--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,80 +1,97 @@
-import {useState} from "react";
+import { useState } from "react";
 import Button from "../../components/Button";
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useUser } from "../../utility/UserProvider";
 import Input from "../../components/Input";
 import axios from "axios";
 import toast from "react-hot-toast";
 
 export default function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const { user, updateUser } = useUser();
+  const [password, setPassword] = useState("");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleForm = (e) => {
-        e.preventDefault();
-        try {
-            axios.post('/api/login', {email, password})
-                .then(res => {
-                    const error = res.data.error;
-                    if (error) {
-                        toast.error(error);
-                    } else {
-                        console.log(res)
-                    }
-                })
-                .catch(err => console.log(err));
-        } catch (e) {
-            console.log(e);
-        }
+  const handleForm = (e) => {
+    e.preventDefault();
+    try {
+      axios.post("/api/login", { email, password }).then((res) => {
+        const { data } = res;
 
+        // Update user data using the context
+        updateUser(data);
+
+        // You can also access the updated user data from the context
+        console.log("Updated user data:", user);
+
+        navigate("/");
+      });
+    } catch (e) {
+      toast.error(e);
+      console.log(e);
     }
-    return (
-        <>
-            <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
-                <div className="bg-white p-3 rounded w-25">
-                    <h2>Login</h2>
-                    <form onSubmit={handleForm}>
+  };
+  return (
+    <>
+      <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
+        <div className="bg-white p-3 rounded w-25">
+          <h2>Login</h2>
+          <form onSubmit={handleForm}>
+            <Input
+              value={email}
+              label="Email"
+              type="text"
+              placeholder="Enter Email"
+              name="email"
+              className="form-control rounded-0"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
 
-                        <Input
-                            value={email}
-                            label="Email"
-                            type="text"
-                            placeholder="Enter Email"
-                            name="email"
-                            className="form-control rounded-0"
-                            onChange={e => {
-                                setEmail(e.target.value)
-                            }}
-                        />
-
-                        <Input
-                            value={password}
-                            label="Password"
-                            type="password"
-                            placeholder="Enter Password"
-                            name="password"
-                            className="form-control rounded-0"
-                            onChange={e => {
-                                setPassword(e.target.value)
-                            }}
-                        />
-                        <Button text="Signup" color={'blue'}/>
-                        <p>Do not have an account? <br/>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <Link style={{textDecoration: "none"}} to={'/property-management/register'}>Property Manager</Link>
-                                </div>
-                                <div className="col-md-6">
-                                    <Link style={{textDecoration: "none"}} to={'/tenant/register'}>Tenant</Link>
-                                </div>
-                            </div>
-                        </p>
-
-                        <p><Link style={{textDecoration: "none"}} to={'/'}>Go back to homePage</Link></p>
-                    </form>
+            <Input
+              value={password}
+              label="Password"
+              type="password"
+              placeholder="Enter Password"
+              name="password"
+              className="form-control rounded-0"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <Button text="Sign in" color={"blue"} />
+            <p>
+              Do not have an account? <br />
+              <div className="row">
+                <div className="col-md-6">
+                  <Link
+                    style={{ textDecoration: "none" }}
+                    to={"/property-management/register"}
+                  >
+                    Property Manager
+                  </Link>
                 </div>
-            </div>
-        </>
-    );
+                <div className="col-md-6">
+                  <Link
+                    style={{ textDecoration: "none" }}
+                    to={"/tenant/register"}
+                  >
+                    Tenant
+                  </Link>
+                </div>
+              </div>
+            </p>
+
+            <p>
+              <Link style={{ textDecoration: "none" }} to={"/"}>
+                Go back to homePage
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
