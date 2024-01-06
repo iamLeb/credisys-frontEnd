c7aee646140e85199ae4857ba7fd130495d54700
jashanpreet28122001@gmail.com <jashanpreet28122001@gmail.com>
2024-01-05 18:51:17 -0800

    landing-page-basic-setup-done

--- credsis-frontend/src/App.jsx
+++ credsis-frontend/src/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Nav from './components/landing_page/Nav'
+import Hero from './components/landing_page/Hero'
+import Home from './views/Home'
+import { BrowserRouter as Router , Routes, Route, BrowserRouter } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <Router>
+      <Routes>
+      <Route ></Route>
+
+
+      </Routes>
     <>
-       <h1 class="text-5xl   underline bg-slate-600">
-    Hello world!
-  </h1>
+    <div >
+     <Home/>
+</div>
     </>
+    </Router>
   )
 }
 
--- credsis-frontend/tailwind.config.js
+++ credsis-frontend/tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
   content: ["./src/**/*.{html,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'regal-blue': '#243c5a',
+        'primary': '#3F72AF',
+        'secondary': "#112D4E",
+        'primary-2': "#F9F7F7",
+        'secondary-2':"#F0ECE5",
+        'nav-back':"#43766C",
+        'text-1':"#76453B",
+        "button-1":"#092635",
+        "head-cred": "#0174BE",
+        "text-2-background":"#F1B4BB",
+        "desc-color-1":"#F1B4BB"
+
+
+      },
+      fontSize: {
+       xxl:['50px']
+      }
+
+
+    },
   },
   plugins: [],
 }
